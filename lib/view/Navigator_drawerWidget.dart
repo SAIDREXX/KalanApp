450829dac5ac78886b3aece7bb50b7dd106d88a1
SAIDREXX <saidrexxdemher@gmail.com>
2023-07-30 20:12:45 -0600

    Minor Changes

--- lib/view/Navigator_drawerWidget.dart
+++ lib/view/Navigator_drawerWidget.dart
@@ -1,41 +1,36 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kalanapp/constants/colors.dart';
-class NavigatorDrawerWidget extends StatelessWidget{
+
+class NavigatorDrawerWidget extends StatelessWidget {
   final paddig = EdgeInsets.symmetric(horizontal: 20);
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Drawer(
-      child: Material(
-        color: ColorConstants.paletteColor2,
-        child: ListView(
-          children: <Widget>
-          [
-            const SizedBox(height: 48),
-          buildMenuItem(
-              text: 'People',
-            icon: Icons.people
-
-          ),
-          ],
-        ),
-      )
-    );
-
+        child: Material(
+      color: ColorConstants.paletteColor2,
+      child: ListView(
+        children: <Widget>[
+          const SizedBox(height: 48),
+          buildMenuItem(text: 'People', icon: Icons.people),
+        ],
+      ),
+    ));
   }
 }
-
 
 Widget buildMenuItem({
   required String text,
   required IconData icon,
-}){
+}) {
   final color = Colors.white;
   final hoverColor = Colors.white70;
   return ListTile(
-    leading: Icon(icon, color: color,),
-    title: Text(text,style: TextStyle(color: color)),
+    leading: Icon(
+      icon,
+      color: color,
+    ),
+    title: Text(text, style: TextStyle(color: color)),
     hoverColor: hoverColor,
-    onTap: (){},
+    onTap: () {},
   );
 }
