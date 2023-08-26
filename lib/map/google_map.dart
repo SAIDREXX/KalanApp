cd589349587fe1337412c1256552ee2a5db9a382
SAIDREXX <saidrexxdemher@gmail.com>
2023-08-26 16:02:20 -0600

    Google Map V1

---
+++ lib/map/google_map.dart
@@ -0,0 +1,53 @@
+import 'package:location/location.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapsMaps extends StatefulWidget {
+  const GoogleMapsMaps({super.key});
+
+  @override
+  State<GoogleMapsMaps> createState() => _GoogleMapsMapsState();
+}
+
+class _GoogleMapsMapsState extends State<GoogleMapsMaps> {
+  GoogleMapController? mapController;
+  Location location = Location();
+  List<LatLng> routePoints = [];
+
+  @override
+  void initState() {
+    super.initState();
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      if (mapController != null) {
+        mapController!.animateCamera(CameraUpdate.newLatLng(
+            LatLng(currentLocation.latitude!, currentLocation.longitude!)));
+
+        setState(() {
+          routePoints.add(
+              LatLng(currentLocation.latitude!, currentLocation.longitude!));
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(target: LatLng(0, 0), zoom: 15),
+      onMapCreated: (GoogleMapController controller) {
+        mapController = controller;
+      },
+      markers: Set.from(routePoints.map((point) =>
+          Marker(markerId: MarkerId(point.toString()), position: point))),
+      polylines: {
+        Polyline(
+          polylineId: const PolylineId('route'),
+          visible: true,
+          points: routePoints,
+          color: Colors.blue,
+          width: 4,
+        )
+      },
+    );
+  }
+}
--- lib/view/membersMonitorScreens/member1.dart
+++ lib/view/membersMonitorScreens/member1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kalanapp/map/google_map.dart';
 import 'package:kalanapp/utils/following_to_button.dart';
 import 'package:kalanapp/utils/set_status_button.dart';
 
@@ -27,13 +28,18 @@ class _Member1State extends State<Member1> {
               fit: BoxFit.cover,
             ),
           ),
-          child: const Column(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(height: 40),
               FollowingButton(
                 index: 0,
+              ),
+              Container(
+                width: 150,
+                height: 150,
+                child: GoogleMapsMaps(),
               ),
               Spacer(),
               StatusButton(userIndex: 0),
